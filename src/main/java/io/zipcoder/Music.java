8007d47774a3c6903cdb11c0b93a2756e85e048b
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-09 21:05:37 -0400

    cleaned

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -10,8 +10,8 @@ public class Music {
 
     public static Integer selection(Integer startIndex, String selection){
 
-        int countUp = 0;
-        int countDown = 0;
+        int fastForward = 0;
+        int rewind = 0;
 
         for (int i = startIndex; i <= playList.length; i++) {
             if (i == playList.length) {
@@ -19,27 +19,26 @@ public class Music {
             } else if (playList[i].equals(selection)) {
                 break;
             }
-            countUp++;
-            System.out.println("countUp: " + countUp);
+            fastForward++;
+            System.out.println("countUp: " + fastForward);
         }
 
         for (int i = startIndex; i >= 0; i--) {
             if (playList[i].equals(selection)) {
-
                 break;
             } else if (i == 0){
                 i = playList.length;
             }
-            countDown++;
-            System.out.println("countDown: " + countDown);
+            rewind++;
+            System.out.println("countDown: " + rewind);
         }
 
-        if (countUp < countDown) {
-            return countUp;
-        } else if (countUp == countDown) {
-            return countUp;
+        if (fastForward < rewind) {
+            return fastForward;
+        } else if (fastForward == rewind) {
+            return fastForward;
         } else {
-            return countDown;
+            return rewind;
         }
     }
 
