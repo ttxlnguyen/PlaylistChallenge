05725f43ea75462530591066bebf5e2740812eef
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-09 20:52:57 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -2,13 +2,48 @@ package io.zipcoder;
 
 public class Music {
 
-    private String[] playList;
+    private static String[] playList = {"wheniseeyouagain","borntorun","nothingelsematters","cecelia"};
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public static Integer selection(Integer startIndex, String selection){
+
+        int countUp = 0;
+        int countDown = 0;
+
+        for (int i = startIndex; i <= playList.length; i++) {
+            if (i == playList.length) {
+                i = 0;
+            } else if (playList[i].equals(selection)) {
+                break;
+            }
+            countUp++;
+            System.out.println("countUp: " + countUp);
+        }
+
+        for (int i = startIndex; i >= 0; i--) {
+            if (playList[i].equals(selection)) {
+
+                break;
+            } else if (i == 0){
+                i = playList.length;
+            }
+            countDown++;
+            System.out.println("countDown: " + countDown);
+        }
+
+        if (countUp < countDown) {
+            return countUp;
+        } else if (countUp == countDown) {
+            return countUp;
+        } else {
+            return countDown;
+        }
+    }
+
+    public static void main(String[] args){
+        selection(2, "borntorun");
     }
 }
